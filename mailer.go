f6471425beb0241012e2dc4d28671e5146997238
Rob Sliwa <robjsliwa@gmail.com>
2016-11-22 15:28:44 -0500

    Email server now accepts payload and executes on seperate go routines.

--- mailer.go
+++ mailer.go
@@ -7,6 +7,7 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"io"
 	"log"
 	"net/http"
@@ -27,7 +28,7 @@ func handle_signals() {
 	signal.Notify(c, syscall.SIGTERM)
 	go func() {
 		<-c
-		log.Println("counter=authMgrStop, Message=Shutting down auth manager .... ")
+		log.Println("counter=emailRelayStop, Message=Shutting down email relay .... ")
 		os.Exit(1)
 	}()
 }
@@ -39,6 +40,9 @@ func getConfiguration(config_file string) (err error) {
 }
 
 func init() {
+	if len(os.Args) < 2 {
+		log.Fatal("Must pass in configuration file")
+	}
 	err := getConfiguration(os.Args[1])
 	if err != nil {
 		log.Println("Failed to get configuration file.")
@@ -75,17 +79,55 @@ func handleSendEmail(w http.ResponseWriter, r *http.Request) {
 }
 
 func mailrelayhandler(w http.ResponseWriter, r *http.Request) {
-	/*
+	// decode request body for user request key values
+	var objectData map[string]interface{}
+	err := decodeBody(r, &objectData)
 
-		  log.Println(err)
-		  respondErr(w, r, http.StatusUnauthorized, err)
-		  return
+	if err != nil {
+		log.Println("Could not decode request body")
+		respondErr(w, r, http.StatusBadRequest, errors.New("Could not decode request body"))
+		return
+	}
 
-		  respond(w, r, http.StatusOK, map[string]interface{}{
-				"message": "success",
-			})
+	toEmails, ok := objectData["to_emails"].([]interface{})
+	if !ok {
+		log.Println("Could not find to_emails in payload")
+		respondErr(w, r, http.StatusBadRequest, errors.New("Could not find to_emails in payload"))
+		return
+	}
 
-	*/
+	var toEmailsStrings []string
+	for _, email := range toEmails {
+		emailString, ok := email.(string)
+		if !ok {
+			log.Println("to_emails array has wrong type")
+			respondErr(w, r, http.StatusBadRequest, errors.New("to_emails array has wrong type"))
+			return
+		}
+		toEmailsStrings = append(toEmailsStrings, emailString)
+	}
+
+	fromEmail, ok := objectData["from_email"].(string)
+	if !ok {
+		log.Println("Could not find from_email in payload")
+		respondErr(w, r, http.StatusBadRequest, errors.New("Could not find from_email in payload"))
+		return
+	}
+
+	subject, ok := objectData["subject"].(string)
+	if !ok {
+		log.Println("Could not find subject in payload")
+		respondErr(w, r, http.StatusBadRequest, errors.New("Could not find subject in payload"))
+		return
+	}
+
+	emailBody, ok := objectData["email_body"].(string)
+	if !ok {
+		log.Println("Could not find email_body in payload")
+		respondErr(w, r, http.StatusBadRequest, errors.New("Could not find email_body in payload"))
+		return
+	}
+
 	maxRetries, err := strconv.ParseInt(EmailConfigurationInstance().Max_retries, 10, 64)
 	if err != nil {
 		log.Println("Could not get max_retries from configuration file.")
@@ -94,47 +136,58 @@ func mailrelayhandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var currentAttempt int64 = 0
-	isSuccess := false
+	go func() {
+		var currentAttempt int64 = 0
+		isSuccess := false
 
-	for isSuccess == false && currentAttempt < maxRetries {
-		err := sendEmail([]string{"robert_sliwa@comcast.com"}, "donotreply@iris.comcast.com", "Test email", "Test test")
-		if err == nil {
-			log.Println("Email sent")
-			isSuccess = true
-		} else {
-			currentAttempt += 1
+		for isSuccess == false && currentAttempt < maxRetries {
+			err := sendEmail(toEmailsStrings, fromEmail, subject, emailBody)
+			if err == nil {
+				log.Println("Email sent")
+				isSuccess = true
+			} else {
+				currentAttempt += 1
+			}
+			if err != nil {
+				log.Println("Error:", err)
+			}
 		}
-        log.Println("Error:", err)
-	}
+	}()
+
+	respond(w, r, http.StatusOK, map[string]interface{}{
+		"message": "success",
+	})
 }
 
 func sendEmail(toEmails []string, fromEmail, subject, body string) error {
 	// Connect to the remote SMTP server.
-	c, err := smtp.Dial("mailrelay.comcast.com:25")
+	c, err := smtp.Dial(EmailConfigurationInstance().Mailrelay_fqdn)
 	if err != nil {
-        log.Println("Error dialing...")
+		log.Println("Error dialing...")
 		return err
 	}
 	defer c.Close()
-    log.Println("Dialed and sending...")
+	log.Println("Dialed and sending...")
 	// Set the sender and recipient.
-	c.Mail("irisadmin@iris.comcast.com")
-	c.Rcpt("robert_sliwa@comcast.com")
+	c.Mail(fromEmail)
+	for _, toEmail := range toEmails {
+		c.Rcpt(toEmail)
+	}
+
 	// Send the email body.
 	wc, err := c.Data()
 	if err != nil {
-        log.Println("Error executing DATA command...")
+		log.Println("Error executing DATA command...")
 		return err
 	}
 	defer wc.Close()
-    log.Println("Writing DATA...")
-	buf := bytes.NewBufferString("To: robert_sliwa@comcast.com\nSubject: Test with subject\nMIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n<html><body><h1>Test message in html</h1></body></html>")
+	log.Println("Writing DATA...")
+	buf := bytes.NewBufferString("To: " + toEmails[0] + "\nSubject: " + subject + "\nMIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n" + body)
 	if _, err = buf.WriteTo(wc); err != nil {
-        log.Println("Writing DATA...")
+		log.Println("Writing DATA...")
 		return err
 	}
 
-    log.Println("DONE...")
+	log.Println("DONE...")
 	return nil
 }
